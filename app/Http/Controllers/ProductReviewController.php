557dddd8395006c08eae5569242146441debc831
thethlaing-mmdsgit config --global user.name thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2019-11-09 11:12:52 +0630

    Project with vendor folder

---
+++ app/Http/Controllers/ProductReviewController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreReview;
+use App\ProductReview;
+use Illuminate\Http\Request;
+
+class ProductReviewController extends Controller
+{
+    public function index()
+    {
+        $reviews = ProductReview::get();
+        //$reviews = \DB::table('product_reviews')->get();
+        return view('index', compact('reviews'));
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function show(ProductReview $review)
+    {
+        //dd($review);
+        //$review = ProductReview::find($id);
+        //$review = \DB::table('product_reviews')->where('id', $id)->first();
+
+        // if (!$review) {
+        //     abort(404);
+        // }
+        return view('detail', compact('review'));
+    }
+
+    public function store(StoreReview $request)
+    {
+
+        $review = new ProductReview();
+        $review->product_name = $request->product_name;
+        $review->review = $request->review;
+        $review->save();
+
+        // $request_data = $request->all();
+        // unset($request_data['_token']);
+
+        // $result = \DB::table('product_reviews')->insert($request_data);
+
+        // if ($result) {
+            return redirect()->route('product_review.home');
+        // }
+    }
+    public function edit($id)
+    {
+        $review  = \DB::table('product_reviews')->where('id', '=', $id)->first();
+
+        return view('edit', compact('review'));
+    }
+
+    public function update(StoreReview $request, $id)
+    {
+        $request_product = $request->all();
+        unset($request_product['_token']);
+
+        $result = \DB::table('product_reviews')->where('id', $id)->update($request_product);
+
+        if ($result) {
+            return redirect()->route('product_review.show', [$id]);
+        }
+    }
+    public function upvote(ProductReview $review)
+    {
+        $review->increment('votes');
+
+        return redirect()->back();
+    }
+
+    public function downvote(ProductReview $review)
+    {
+        $review->decrement('votes');
+
+        return redirect()->back();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::name('product_review.')->prefix('product_review')->group(function () {
+    Route::get('/all', 'ProductReviewController@index')->name('home');
+
+    Route::get('/create', 'ProductReviewController@create')->name('create');
+    Route::post('/store', 'ProductReviewController@store')->name('store');
+
+    Route::get('/{id}/edit', 'ProductReviewController@edit')->name('edit');
+    Route::post('/{id}/update', 'ProductReviewController@update')->name('update');
+
+    Route::post('/{review}/upvote', 'ProductReviewController@upvote')->name('upvote');
+    Route::post('/{review}/downvote', 'ProductReviewController@downvote')->name('downvote');
+    Route::get('/{review}', 'ProductReviewController@show')->name('show');
+});
